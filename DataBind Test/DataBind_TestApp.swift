369f6dc1e873e213602e016de5e0d09f818afac8
Afeez <harfyzz@gmail.com>
2025-04-27 09:49:32 +0100

    Initial Commit

---
+++ DataBind Test/DataBind_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataBind_TestApp.swift
+//  DataBind Test
+//
+//  Created by Afeez Yunus on 27/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct DataBind_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
